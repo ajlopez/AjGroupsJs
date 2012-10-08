1edc7576c57651ab64ddfece5710c0bff56cba4c
ajlopez <webmaster@ajlopez.com>
2012-10-08 17:45:09 -0300

    Multiply permutation with first test

--- test/permutations.js
+++ test/permutations.js
@@ -6,3 +6,10 @@ var ajgroups = require('../'),
 
 var perm1 = ajgroups.permutation([1, 0]);
 assert.ok(perm1);
+
+// Multiply to get identity
+
+var perm2 = perm1.multiply(perm1);
+assert.ok(perm2);
+assert.equal(perm2.isIdentity(), true);
+
