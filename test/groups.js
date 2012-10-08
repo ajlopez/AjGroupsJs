422f5bc344d83927909424d61758d9b406d91e86
ajlopez <webmaster@ajlopez.com>
2012-10-08 19:15:59 -0300

    get elements with filter, first tests on order

--- test/groups.js
+++ test/groups.js
@@ -8,3 +8,27 @@ var symmetric3 = ajgroups.symmetric(3);
 assert.ok(symmetric3);
 assert.equal(symmetric3.order(), 3*2*1);
 
+// Symmetric 4
+
+var symmetric4 = ajgroups.symmetric(4);
+assert.ok(symmetric4);
+assert.equal(symmetric4.order(), 4*3*2*1);
+
+// Get identity
+
+var elements = symmetric3.elements(function(el) { return el.order() == 1 });
+assert.ok(elements);
+assert.equal(elements.length, 1);
+assert.equal(elements[0].isIdentity(), true);
+
+// Get order 2
+
+var elements = symmetric3.elements(function(el) { return el.order() == 2 });
+assert.ok(elements);
+assert.equal(elements.length, 3);
+
+// Get order 3
+
+var elements = symmetric3.elements(function(el) { return el.order() == 3 });
+assert.ok(elements);
+assert.equal(elements.length, 2);
