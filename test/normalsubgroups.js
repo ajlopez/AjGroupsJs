06703b4f39e1b4991193c1c92aa515a8b0e2f815
ajlopez <webmaster@ajlopez.com>
2020-08-27 12:04:10 -0300

    Normal subgroups tests refactor

--- test/normalsubgroups.js
+++ test/normalsubgroups.js
@@ -1,37 +1,37 @@
 
-var ajgroups = require('../'),
+const ajgroups = require('../'),
     assert = require('assert');
 
 // Normal Subgroups contains identity and original group
 
-var symmetric3 = ajgroups.symmetric(3);
-var id = ajgroups.createGroup([0]);
+const symmetric3 = ajgroups.symmetric(3);
+const id = ajgroups.createGroup([0]);
 
-var subgroups = symmetric3.normalSubgroups();
+const subgroups = symmetric3.normalSubgroups();
 
 assert.equal(contains(subgroups, id), true);
 assert.equal(contains(subgroups, symmetric3), true);
 
 // Cyclic group with prime order has 2 normal subgroups
 
-var cyclic = ajgroups.createGroup([1, 2, 0]);
-var subgroups = cyclic.normalSubgroups();
-assert.equal(subgroups.length, 2);
+const cyclic = ajgroups.createGroup([1, 2, 0]);
+const subgroups2 = cyclic.normalSubgroups();
+assert.equal(subgroups2.length, 2);
 
 // Cyclic group order 6
 
-var cyclic = ajgroups.createGroup(ajgroups.cyclic(6));
-var subgroups = cyclic.normalSubgroups();
-assert.equal(subgroups.length, 4);
+const cyclic2 = ajgroups.createGroup(ajgroups.cyclic(6));
+const subgroups3 = cyclic2.normalSubgroups();
+assert.equal(subgroups3.length, 4);
 
 // Symmetric 3
 
-var group = ajgroups.symmetric(3);
-var subgroups = group.normalSubgroups();
-assert.equal(subgroups.length, 3);
+const group = ajgroups.symmetric(3);
+const subgroups4 = group.normalSubgroups();
+assert.equal(subgroups4.length, 3);
 
 function contains(elements, element) {
-    for (var n in elements) {
+    for (let n in elements) {
         if (elements[n].equals(element))
             return true;
     }
