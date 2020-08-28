40d25a4fba1b26b1fb02c262860b09eb5fe9cefd
ajlopez <webmaster@ajlopez.com>
2020-08-28 11:04:59 -0300

    Groups tests refactor

--- test/groups.js
+++ test/groups.js
@@ -1,58 +1,58 @@
 
-var ajgroups = require('../'),
-    assert = require('assert');
+const ajgroups = require('../');
+const assert = require('assert');
     
 // Symmetric 1
 
-var symmetric1 = ajgroups.symmetric(1);
+const symmetric1 = ajgroups.symmetric(1);
 assert.ok(symmetric1);
 assert.equal(symmetric1.order(), 1);
     
 // Symmetric 2
 
-var symmetric2 = ajgroups.symmetric(2);
+const symmetric2 = ajgroups.symmetric(2);
 assert.ok(symmetric2);
 assert.equal(symmetric2.order(), 2*1);    
 
 // Symmetric 3
 
-var symmetric3 = ajgroups.symmetric(3);
+const symmetric3 = ajgroups.symmetric(3);
 assert.ok(symmetric3);
 assert.equal(symmetric3.order(), 3*2*1);
 
 // Symmetric 4
 
-var symmetric4 = ajgroups.symmetric(4);
+const symmetric4 = ajgroups.symmetric(4);
 assert.ok(symmetric4);
 assert.equal(symmetric4.order(), 4*3*2*1);
 
 // Get identity
 
-var elements = symmetric3.elements(function(el) { return el.order() == 1 });
+const elements = symmetric3.elements(function(el) { return el.order() == 1 });
 assert.ok(elements);
 assert.equal(elements.length, 1);
 assert.equal(elements[0].isIdentity(), true);
 
-var elements = symmetric4.elements(function(el) { return el.order() == 1 });
-assert.ok(elements);
-assert.equal(elements.length, 1);
-assert.equal(elements[0].isIdentity(), true);
+const elements2 = symmetric4.elements(function(el) { return el.order() == 1 });
+assert.ok(elements2);
+assert.equal(elements2.length, 1);
+assert.equal(elements2[0].isIdentity(), true);
 
 // Get order 2
 
-var elements = symmetric3.elements(function(el) { return el.order() == 2 });
-assert.ok(elements);
-assert.equal(elements.length, 3);
+const elements3 = symmetric3.elements(function(el) { return el.order() == 2 });
+assert.ok(elements3);
+assert.equal(elements3.length, 3);
 
-var elements = symmetric4.elements(function(el) { return el.order() == 2 });
-assert.ok(elements);
-assert.equal(elements.length, 9);
+const elements4 = symmetric4.elements(function(el) { return el.order() == 2 });
+assert.ok(elements4);
+assert.equal(elements4.length, 9);
 
 // Get order 3
 
-var elements = symmetric3.elements(function(el) { return el.order() == 3 });
-assert.ok(elements);
-assert.equal(elements.length, 2);
+const elements5 = symmetric3.elements(function(el) { return el.order() == 3 });
+assert.ok(elements5);
+assert.equal(elements5.length, 2);
 
 // Is subgroup
 
