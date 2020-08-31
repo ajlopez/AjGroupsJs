8f9739d8870a1ab4e04fd842368e84d1c26cee79
ajlopez <webmaster@ajlopez.com>
2020-08-31 11:21:08 -0300

    Group multiply tests refactor

--- test/groupmultiply.js
+++ test/groupmultiply.js
@@ -1,21 +1,26 @@
 
-var ajgroups = require('../'),
-    assert = require('assert');
+const ajgroups = require('../');
+const assert = require('assert');
 
 // Group multiply should be idempotent
 
-var group = ajgroups.symmetric(3);
-var result = group.multiply(group);
-assert.ok(result);
-result.equals(group);
-group.equals(result);
+{
+    const group = ajgroups.symmetric(3);
+    const result = group.multiply(group);
+    assert.ok(result);
+    result.equals(group);
+    group.equals(result);
+}
 
 // Generate symmetric group
 
-var group1 = ajgroups.createGroup([1, 2, 3, 0]);
-var group2 = ajgroups.createGroup([1, 0, 2, 3]);
+{
+    const group1 = ajgroups.createGroup([1, 2, 3, 0]);
+    const group2 = ajgroups.createGroup([1, 0, 2, 3]);
 
-var result = group1.multiply(group2);
+    const result = group1.multiply(group2);
 
-assert.equal(result.order(), 4*3*2*1);
-assert.equal(result.equals(ajgroups.symmetric(4)), true);
+    assert.equal(result.order(), 4*3*2*1);
+    assert.equal(result.equals(ajgroups.symmetric(4)), true);
+}
+
