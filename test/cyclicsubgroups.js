a8cb7d276068872d82b8cecd15349334a1d31915
ajlopez <webmaster@ajlopez.com>
2020-09-01 12:15:34 -0300

    Cyclic groups tests refactor

--- test/cyclicsubgroups.js
+++ test/cyclicsubgroups.js
@@ -1,24 +1,31 @@
 
-var ajgroups = require('../'),
-    assert = require('assert');
+const ajgroups = require('../');
+const assert = require('assert');
 
 // Get cyclic subgroups of S1
 
-var s1 = ajgroups.symmetric(1);
-var cyclicSubgroups = s1.cyclicSubgroups();
-assert.ok(cyclicSubgroups);
-assert.equal(cyclicSubgroups.length, 1);
+{
+    const s1 = ajgroups.symmetric(1);
+    const cyclicSubgroups = s1.cyclicSubgroups();
+    assert.ok(cyclicSubgroups);
+    assert.equal(cyclicSubgroups.length, 1);
+}
 
 // Get cyclic subgroups of S2
 
-var s1 = ajgroups.symmetric(2);
-var cyclicSubgroups = s1.cyclicSubgroups();
-assert.ok(cyclicSubgroups);
-assert.equal(cyclicSubgroups.length, 2);
+{
+    const s1 = ajgroups.symmetric(2);
+    const cyclicSubgroups = s1.cyclicSubgroups();
+    assert.ok(cyclicSubgroups);
+    assert.equal(cyclicSubgroups.length, 2);
+}
 
 // Get cyclic subgroups of S3
 
-var s1 = ajgroups.symmetric(3);
-var cyclicSubgroups = s1.cyclicSubgroups();
-assert.ok(cyclicSubgroups);
-assert.equal(cyclicSubgroups.length, 5);
+{
+    const s1 = ajgroups.symmetric(3);
+    const cyclicSubgroups = s1.cyclicSubgroups();
+    assert.ok(cyclicSubgroups);
+    assert.equal(cyclicSubgroups.length, 5);
+}
+
