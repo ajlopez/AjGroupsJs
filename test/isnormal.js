1fdf00f5fa62f9c31460c7d4d56016f0a9c6689d
ajlopez <webmaster@ajlopez.com>
2020-09-03 10:18:20 -0300

    Is normal test refactor

--- test/isnormal.js
+++ test/isnormal.js
@@ -1,13 +1,16 @@
 
-var ajgroups = require('../'),
-    assert = require('assert');
+const ajgroups = require('../');
+const assert = require('assert');
     
 // Identity and Symmetric groups are normals
 
-for (var k = 1; k <= 5; k++) {
-    var id = ajgroups.createGroup(ajgroups.identity(k));
-    var group = ajgroups.symmetric(k);
-    assert.equal(id.isNormalSubgroup(group), true);
-    assert.equal(group.isNormalSubgroup(group), true);
+{
+    for (let k = 1; k <= 5; k++) {
+        const id = ajgroups.createGroup(ajgroups.identity(k));
+        const group = ajgroups.symmetric(k);
+        
+        assert.equal(id.isNormalSubgroup(group), true);
+        assert.equal(group.isNormalSubgroup(group), true);
+    }
 }
 
