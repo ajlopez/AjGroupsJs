398634a95de07edafd31d01c5e6d4f401c83d431
ajlopez <webmaster@ajlopez.com>
2020-09-14 10:56:15 -0300

    Refactor create group tests to use simpleunit

--- test/creategroup.js
+++ test/creategroup.js
@@ -2,55 +2,45 @@
 const ajgroups = require('../');
 const assert = require('assert');
 
-// Create group using identity array
-
-{
+exports['Create group using identity array'] = function (test) {
     const group = ajgroups.createGroup([0]);
     
-    assert.ok(group);
-    assert.equal(group.order(), 1);
+    test.ok(group);
+    test.equal(group.order(), 1);
 }
 
-// Create group using identity array length 2
-
-{
+exports['Create group using identity array length 2'] = function (test) {
     const group = ajgroups.createGroup([0, 1]);
     const groupb = ajgroups.createGroup([0]);
     
-    assert.ok(group);
-    assert.equal(group.order(), 1);
-    assert.equal(groupb.equals(group), true);
-    assert.equal(group.equals(groupb), true);
+    test.ok(group);
+    test.equal(group.order(), 1);
+    test.equal(groupb.equals(group), true);
+    test.equal(group.equals(groupb), true);
 }
 
-// Create group using an exchange permutation
-
-{
+exports['Create group using an exchange permutation'] = function (test) {
     const group = ajgroups.createGroup([1, 0]);
     
-    assert.ok(group);
-    assert.equal(group.order(), 2);
+    test.ok(group);
+    test.equal(group.order(), 2);
 }
 
-// Create group using exchange and cyclic permutation
-
-{
+exports['Create group using exchange and cyclic permutation'] = function (test) {
     const group = ajgroups.createGroup([1, 0, 2], [1, 2, 0]);
     
-    assert.ok(group);
-    assert.equal(group.order(), 6);
-    assert.ok(group.equals(ajgroups.symmetric(3)));
+    test.ok(group);
+    test.equal(group.order(), 6);
+    test.ok(group.equals(ajgroups.symmetric(3)));
 }
 
-// Groups created from prime order cyclic permutation are equals
-
-{
+exports['Groups created from prime order cyclic permutation are equals'] = function (test) {
     const cyclic = ajgroups.cyclic(5);
     const cyclic2 = cyclic.multiply(cyclic);
     const groupa = ajgroups.createGroup(cyclic);
     const groupb = ajgroups.createGroup(cyclic2);
     
-    assert.equal(groupa.equals(groupb), true);
-    assert.equal(groupb.equals(groupa), true);
+    test.equal(groupa.equals(groupb), true);
+    test.equal(groupb.equals(groupa), true);
 }
 
