fcd0baca7febb33db93090b9dbd2dce475ae9b8d
ajlopez <webmaster@ajlopez.com>
2020-09-20 10:38:11 -0300

    Refactor subgroups tests to use simpleunit

--- test/subgroups.js
+++ test/subgroups.js
@@ -1,80 +1,63 @@
 
 const ajgroups = require('../');
-const assert = require('assert');
 
-// Subgroups contains identity and original group
-
-{
+exports['Subgroups contains identity and original group'] = function (test) {
     const symmetric3 = ajgroups.symmetric(3);
     const id = ajgroups.createGroup([0]);
 
     const subgroups = symmetric3.subgroups();
 
-    assert.equal(contains(subgroups, id), true);
-    assert.equal(contains(subgroups, symmetric3), true);
+    test.equal(contains(subgroups, id), true);
+    test.equal(contains(subgroups, symmetric3), true);
 }
 
-// Cyclic group with prime order has 2 subgroups
-
-{
+exports['Cyclic group with prime order has 2 subgroups'] = function (test) {
     const cyclic = ajgroups.createGroup([1, 2, 0]);
     const subgroups = cyclic.subgroups();
     
-    assert.equal(subgroups.length, 2);
+    test.equal(subgroups.length, 2);
 }
 
-// Cyclic group order 6
-
-{
+exports['Cyclic group order 6'] = function (test) {
     const cyclic = ajgroups.createGroup(ajgroups.cyclic(6));
     const subgroups = cyclic.subgroups();
     
-    assert.equal(subgroups.length, 4);
+    test.equal(subgroups.length, 4);
 }
 
-// Symmetric 3
-
-{
+exports['Symmetric 3'] = function (test) {
     const group = ajgroups.symmetric(3);
     const subgroups = group.subgroups();
     
-    assert.equal(subgroups.length, 6);
+    test.equal(subgroups.length, 6);
 }
 
-// Symmetric 4
-
-{
+exports['Symmetric 4'] = function (test) {
     const group = ajgroups.symmetric(4);
     const subgroups = group.subgroups();
 
-    assert.equal(subgroups.length, 30);
+    test.equal(subgroups.length, 30);
 }
 
-// Cyclic 4
-
-{
+exports['Cyclic 4'] = function (test) {
     const group = ajgroups.createGroup(ajgroups.cyclic(4));
     const subgroups = group.subgroups();
     
-    assert.equal(subgroups.length, 3);
+    test.equal(subgroups.length, 3);
 }
 
-// Cyclic 5
-
-{
+exports['Cyclic 5'] = function (test) {
     const group = ajgroups.createGroup(ajgroups.cyclic(5));
     const subgroups = group.subgroups();
     
-    assert.equal(subgroups.length, 2);
+    test.equal(subgroups.length, 2);
 }
 
-// Cyclic 7
-
-{
+exports['Cyclic 7'] = function (test) {
     const group = ajgroups.createGroup(ajgroups.cyclic(7));
     const subgroups = group.subgroups();
     
-    assert.equal(subgroups.length, 2);
+    test.equal(subgroups.length, 2);
 }
 
 function contains(elements, element) {
