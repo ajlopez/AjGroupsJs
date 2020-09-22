f53b0306458476083942f9532e8f4bff7f5810b2
ajlopez <webmaster@ajlopez.com>
2020-09-22 10:49:36 -0300

    Groups tests refactor to use simpleunit

--- test/groups.js
+++ test/groups.js
@@ -1,112 +1,93 @@
 
 const ajgroups = require('../');
-const assert = require('assert');
     
-// Symmetric 1
-
-{
+exports['Symmetric 1'] = function (test) {
     const symmetric = ajgroups.symmetric(1);
     
-    assert.ok(symmetric);
-    assert.equal(symmetric.order(), 1);
+    test.ok(symmetric);
+    test.equal(symmetric.order(), 1);
 }
     
-// Symmetric 2
-
-{
+exports['Symmetric 2'] = function (test) {
     const symmetric = ajgroups.symmetric(2);
     
-    assert.ok(symmetric);
-    assert.equal(symmetric.order(), 2*1);
+    test.ok(symmetric);
+    test.equal(symmetric.order(), 2*1);
 }
 
-// Symmetric 3
-
-{
+exports['Symmetric 3'] = function (test) {
     const symmetric = ajgroups.symmetric(3);
     
-    assert.ok(symmetric);
-    assert.equal(symmetric.order(), 3*2*1);
+    test.ok(symmetric);
+    test.equal(symmetric.order(), 3*2*1);
 }
 
-// Symmetric 4
-
-{
+exports['Symmetric 4'] = function (test) {
     const symmetric = ajgroups.symmetric(4);
     
-    assert.ok(symmetric);
-    assert.equal(symmetric.order(), 4*3*2*1);
+    test.ok(symmetric);
+    test.equal(symmetric.order(), 4*3*2*1);
 }
 
-// Get identity
-
-{
+exports['Get identity'] = function (test) {
     const symmetric = ajgroups.symmetric(3);
     const elements = symmetric.elements(function(el) { return el.order() == 1 });
     
-    assert.ok(elements);
-    assert.equal(elements.length, 1);
-    assert.equal(elements[0].isIdentity(), true);
+    test.ok(elements);
+    test.equal(elements.length, 1);
+    test.equal(elements[0].isIdentity(), true);
 }
 
-{
+exports['Get identity from Symmetric 4'] = function (test) {
     const symmetric = ajgroups.symmetric(4);
     const elements = symmetric.elements(function(el) { return el.order() == 1 });
 
-    assert.ok(elements);
-    assert.equal(elements.length, 1);
-    assert.equal(elements[0].isIdentity(), true);
+    test.ok(elements);
+    test.equal(elements.length, 1);
+    test.equal(elements[0].isIdentity(), true);
 }
 
-// Get order 2
-
-{
+exports['Get order 2'] = function (test) {
     const symmetric = ajgroups.symmetric(3);
     const elements = symmetric.elements(function(el) { return el.order() == 2 });
     
-    assert.ok(elements);
-    assert.equal(elements.length, 3);
+    test.ok(elements);
+    test.equal(elements.length, 3);
 }
 
-{
+exports['Get Order 2 from Symmetric 4'] = function (test) {
     const symmetric = ajgroups.symmetric(4);
     const elements = symmetric.elements(function(el) { return el.order() == 2 });
     
-    assert.ok(elements);
-    assert.equal(elements.length, 9);
+    test.ok(elements);
+    test.equal(elements.length, 9);
 }
 
-// Get order 3
-
-{
+exports['Get order 3'] = function (test) {
     const symmetric = ajgroups.symmetric(3);
     const elements = symmetric.elements(function(el) { return el.order() == 3 });
     
-    assert.ok(elements);
-    assert.equal(elements.length, 2);
+    test.ok(elements);
+    test.equal(elements.length, 2);
 }
 
-// Is subgroup
-
-{
+exports['Is subgroup'] = function (test) {
     const symmetric2 = ajgroups.symmetric(2);
     const symmetric3 = ajgroups.symmetric(3);
     const symmetric4 = ajgroups.symmetric(4);
     
-    assert.equal(symmetric3.isSubgroup(symmetric4), true);
-    assert.equal(symmetric2.isSubgroup(symmetric4), true);
-    assert.equal(symmetric2.isSubgroup(symmetric3), true);
-    assert.equal(symmetric4.isSubgroup(symmetric3), false);
+    test.equal(symmetric3.isSubgroup(symmetric4), true);
+    test.equal(symmetric2.isSubgroup(symmetric4), true);
+    test.equal(symmetric2.isSubgroup(symmetric3), true);
+    test.equal(symmetric4.isSubgroup(symmetric3), false);
 }
 
-// equals
-
-{
+exports['equals'] = function (test) {
     const symmetric3 = ajgroups.symmetric(3);
     const symmetric4 = ajgroups.symmetric(4);
     
-    assert.equal(symmetric3.equals(symmetric3), true);
-    assert.equal(symmetric3.equals(symmetric4), false);
-    assert.equal(symmetric3.equals(ajgroups.symmetric(3)), true);
+    test.equal(symmetric3.equals(symmetric3), true);
+    test.equal(symmetric3.equals(symmetric4), false);
+    test.equal(symmetric3.equals(ajgroups.symmetric(3)), true);
 }
 
